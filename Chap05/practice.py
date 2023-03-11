54d255c04e76a2f99442037733316b5797757f06
Anh Tran <anhtr077@gmail.com>
2023-03-11 14:02:13 -0800

    origin

---
+++ Chap05/practice.py
@@ -0,0 +1,8 @@
+def favorite_city (name):
+    name == "Seattle, Santa Barbara, Boulder"
+    print("One of my favorite cities is", name)
+
+favorite_city("Seattle, Vancouver")
+favorite_city("Santa Barbara")
+favorite_city("Boulder")
+    
